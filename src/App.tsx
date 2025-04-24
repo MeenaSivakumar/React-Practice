9a69e185f6d044021fa324f2953b62998652d80a
MeenaSivakumar <meenasad09@gmail.com>
2025-04-24 19:32:23 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-
-import './App.css';
-import { ArrayState } from './Practice/ArrayInState/ArrayState';
-import { ConditionalRendoring } from './Practice/CondtionalRendering/ConditionalRendering';
-import { DefaultProp } from './Practice/DefaultProps/DefaultProp';
-import { DisableButton } from './Practice/DisableButton/DisableButton';
-import { Count } from './Practice/DisplayCount/Count';
-import { Input } from './Practice/DisplayInput';
-import { UserInfo } from './Practice/DisplayObject';
-import { CallBackComponent } from './Practice/FunctionComponent/CallBackComponent';
-import { IterateItems } from './Practice/IterateList/IterateItems';
-import { MultipleState } from './Practice/MultipleState/MultipleState';
-import { Container } from './Practice/RenderChildren/Container';
-import { ResetButton } from './Practice/ResetButton/ResetButton';
-import { Button } from './Practice/ReusableBuuton/Buuton';
-import { ToggleButton } from './Practice/ToggleButton';
-import { UpdateUserObject } from './Practice/UpdateObject/UpdateUserObject';
-import { ApiFetch } from './Practice/UseEffect/ApiFetch';
-import { CleanUp } from './Practice/UseEffect/CleanUp';
-import { CopMount } from './Practice/UseEffect/ComMount';
-import { CountUpdate } from './Practice/UseEffect/CountUpdate';
-import { EffectOnSpecific } from './Practice/UseEffect/EffectOnSpecificConditon';
-import { EffectWithInput } from './Practice/UseEffect/EffectWithInput';
-import { MultipleUseEffect } from './Practice/UseEffect/MultipleUseEffects';
-import { UpdateObjectUseEffect } from './Practice/UseEffect/UpdateObjectUseEffect';
-import { UpdateTitle } from './Practice/UseEffect/UpdateTitle';
+import { Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import { UpdateObjectUseEffect } from "./Practice/UseEffect/UpdateObjectUseEffect";
 
 function App() {
-  const func = ()=>{
+  const func = () => {
     console.log("button clicked");
-  }
+  };
   return (
+   
+    
+ 
     <div >
      {/* <DisplayString message="hello react......"/> */}
      {/* <Count/> */}
@@ -56,6 +36,7 @@ function App() {
      {/* <ApiFetch/> */}
      {/* <EffectOnSpecific/> */}
      <UpdateObjectUseEffect/>
+     {/* <LoginForm/> */}
     </div>
   );
 }
